4abb19bca45f4399b79321cfa2ba03bd0199f207
Joachim Kliemann <joachim.kliemann@googlemail.com>
2024-10-09 23:03:07 +0200

    add global styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
 
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-
-  ["src/favicon.ico", "src/opengraph.jpg"].forEach((item) =>
-    eleventyConfig.addPassthroughCopy(item)
-  );
-
   return {
     dir: {
       input: "src",
